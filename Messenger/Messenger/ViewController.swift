a424b1e283c1e960b4dc81b82c93e0a6a93db03f
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2022-05-17 11:17:33 +0300

    Getting Started

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Никита Пивоваров on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
